84513d9643446c2392caba33450bcda5a8c47abc
Leonid-spec <winneraftersales@gmail.com>
2024-12-12 12:51:43 +0100

    new

--- HomeWork14/script.js
+++ HomeWork14/script.js
@@ -13,56 +13,128 @@
 //        }
 
 let bankAccount = {
-    accountNumber: "123456789",
-    accountHolderName: "Alice",
-    balance: 0
-}
+  accountNumber: "123456789",
+  accountHolderName: "Alice",
+  balance: 0,
+};
 
+bankAccount.deposit = function (sum) {
+  sum >= 5 && sum <= 5000 ? (this.balance += sum) : alert("Uncorrect sum");
+};
 
-bankAccount.deposit = function(sum) {
-    sum >= 5 && sum <= 5000 ? this.balance +=sum : alert("Uncorrect sum")
-}
+bankAccount.withdraw = function (sum) {
+  sum <= this.balance && sum > 0
+    ? (this.balance -= sum)
+    : alert("Uncorrect sum");
+};
 
-bankAccount.withdraw = function(sum){
-    sum <= this.balance && sum > 0 ? this.balance -=sum : alert("Uncorrect sum")
-}
-
-bankAccount.checkbalance = function(){
-    console.log(`Your balance is not so bed: ${this.balance}`);
-    
-}
+bankAccount.checkbalance = function () {
+  console.log(`Your balance is not so bed: ${this.balance}`);
+};
 
 const bank = [];
 
-
 function createAccount() {
-    const nameInput = document.getElementById('name');
-    const name = nameInput.value.trim();
+  const nameInput = document.getElementById("name");
+  const name = nameInput.value.trim();
 
-    // falsy null, '', 0, undefined, NaN
-    if (name){
-bank.push({
-    ...bankAccount,
-    accountNumber: bank.length + 1,
-    accountHolderName: name
-})
-alert('Account created successfully')
-    } else {
-        alert('Please, enter a valid name')
-    }
-    nameInput.value = '';
-    console.log(bank);
-    
-
+  // falsy null, '', 0, undefined, NaN
+  if (name) {
+    bank.push({
+      ...bankAccount,
+      accountNumber: bank.length + 1,
+      accountHolderName: name,
+    });
+    alert("Account created successfully");
+  } else {
+    alert("Please, enter a valid name");
+  }
+  nameInput.value = "";
+  console.log(bank);
 }
 
-function showAccounts() {
-   if (bank.length === 0){
-    alert('Accounts list is empty')
-   }else { let accountsList = 'Accounts:\n'; 
-   for (let account of bank)
-     { accountsList += `Account Number: ${account.accountNumber}, Account Holder: ${account.accountHolderName}, Balance: ${account.balance}\n`; }
-    alert(accountsList);
-}
+// function showAccounts() {
+//   if (bank.length === 0) {
+//     alert("Accounts list is empty");
+//   } else {
+//     let accountsList = "Accounts:\n";
+//     for (let account of bank) {
+//       accountsList += `Account Number: ${account.accountNumber}, Account Holder: ${account.accountHolderName}, Balance: ${account.balance}\n`;
+//     }
+//     alert(accountsList);
+//   }
+// }
+
+
+
+function showAccounts(){
+const accountList = document.getElementById('accountList');
+
+ accountList.innerHTML = '';
+
+  for (const account of bank) {
+
+accountList.innerHTML += `
+<li>ID: ${account.accountNumber}, Name: ${account.accountHolderName}, Balance: ${account.balance}</li>`
+  } 
 }
 
+const withdraw = document.getElementById('withdraw');
+const deposit = document.getElementById('deposit');
+
+
+deposit.onclick = function(){
+  operation('deposit');
+//   const accountIdInput = document.getElementById("accountId"); //поолучили ссылки на элементы
+//   const amountInput = document.getElementById("amount");
+
+//   const accountId = accountIdInput.value.trim(); //считывание  значение написанное в инпутах, число либо НАН
+//   const amount = +amountInput.value.trim(); //получить значение 
+
+
+
+// // Искомый элемент или undefined
+//  const accountFind = bank.find(e => e.accountNumber.toString() === accountId);
+
+// if (accountFind){
+//   accountFind.deposit(amount);
+// }else{
+//   alert('Account not found');
+// }
+
+// accountIdInput.value = '';
+// amountInput.value = '';
+
+};
+
+withdraw.onclick = function(){
+operation('withdraw');
+
+};
+
+
+// DRY (Don`t repeat yourself)
+
+function operation(operator){
+  const accountIdInput = document.getElementById("accountId");  
+  const amountInput = document.getElementById("amount");
+
+  const accountId = accountIdInput.value.trim(); 
+  const amount = +amountInput.value.trim();  
+
+ const accountFind = bank.find(e => e.accountNumber.toString() === accountId);
+
+if (accountFind){
+  if (operator === 'deposit'){
+    accountFind.deposit(amount);
+  } else {
+    accountFind.withdraw(amount);
+  } }else {
+  alert('Account not found');
+}
+  
+
+accountIdInput.value = '';
+amountInput.value = "";
+
+};
